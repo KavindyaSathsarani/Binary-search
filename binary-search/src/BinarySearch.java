7ca45101e6c6e1d290fa62bd8c43a6c6e4dc7acb
[KavindyaSathsarani] <[sathsaranigunawardhana@gmail.com]>
2020-10-13 23:18:26 +0530

    Update BinarySearch.java

--- binary-search/src/BinarySearch.java
+++ binary-search/src/BinarySearch.java
@@ -7,6 +7,7 @@ class BinarySearch {
       Scanner input = new Scanner(System.in);
       System.out.println("Enter number of elements:");
       num = input.nextInt();
+      array = new int[num];
     }
     
 }
